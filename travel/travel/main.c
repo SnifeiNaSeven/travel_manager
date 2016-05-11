6ea00e909c0921ef359622afddbbed387cf1b32a
Gonçalo Amaral <Gonalo Amaral>
2016-05-11 02:57:25 +0100

    Continuaçao da funçao 1

--- travel/travel/main.c
+++ travel/travel/main.c
@@ -17,6 +17,10 @@ void clrscr()
     system("@cls||clear");
 }
 
+void clean_str(char *str){
+    memset (str,' ',strlen(str));
+}
+
 int menu_principal(void) {
     /*
     Menu
@@ -54,13 +58,16 @@ int menu_principal(void) {
 
 int menu_1(void) {
     FILE *viagens = fopen("viagens-datas.txt","r");
-    FILE *cod_viagens = fopen("viagens-cod.txt","r")
+    FILE *cod_viagens = fopen("viagens-cod.txt","r");
     char line[1024];
+    char aux[128];
     int i=0;
     int j;
     int option;
+    Viagem new_node= malloc(sizeof(Viagem_node));
 
     printf("Adquirir uma viagem\n");
+
     while(fgets(line,1024,viagens)){
         if(*line!='-'&&*line!='\n') {
             ++i;
@@ -84,6 +91,61 @@ int menu_1(void) {
         printf("Viagem invalida!\n\n");
         menu_1();
     }
+
+    j=1;
+    clrscr();
+    while(fgets(line,1024,cod_viagens)){
+        if(j==option) {
+            break;
+        }else ++j;
+    }
+
+    j=0;
+    while( *(line+j) ){
+        if( *(line+j) == ' ') break;
+        aux[j]=*(line+j);
+        ++j;
+    }
+
+    new_node->cod_destino=atoi(aux);
+    clrscr();
+
+    data:
+    i=0;
+    fseek (viagens, 0L, SEEK_SET);
+    printf("Data:\n");
+    while(fgets(line,1024,viagens)){/*iterar linhas*/
+        if(*line!='-'&&*line!='\n'){
+            j=0;
+            clean_str(aux);
+            while(*(line+j)!=' '){
+                aux[j]=*(line+j);
+                ++j;
+            }
+            option = atoi(aux);/*codigo capturado*/
+            if(new_node->cod_destino == option) {
+                j=1;
+                fgets(line,1024,viagens);
+                while(*line != '\n'){/*Entrar seccao*/
+                    ++i;
+                    printf("%d)%c%c/%c%c/%c%c%c%c\n",i,line[1],line[2],line[3],line[4],line[5],line[6],line[7],line[8]);
+                    fgets(line,1024,viagens);
+                }
+            }
+        }
+    }
+    printf("\nOpcao: ");
+    scanf("%d",&option);
+    if(option>i || option< 1) {
+        clrscr();
+        printf("Data invalida!\n\n");
+        goto data;
+    }
+
+
+
+
+
     fclose(viagens);
     fclose(cod_viagens);
 }
@@ -92,16 +154,16 @@ int main()
 {
     /*Menu*/
     int option = menu_principal();
-
+    switch(option) {
     /*(1)Adquirir uma viagem*/
-    clrscr();
-    int out = menu_1();
+    case 1:
+        clrscr();
+        int out = menu_1();
 
-
+        break;
     /*(2)Colocar em fila de espera para uma viagem*/
-
-
-
+    case 2: ;
+        break;
     /*(3)Cancelar viagem*/
 
 
@@ -120,8 +182,6 @@ int main()
 
     /*(7)Listar Clientes*/
 
-
-
-
+    }
     return 0;
 }
